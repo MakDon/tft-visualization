8b3c8f3a8e0ab1f6de16262d7a224dc827bd9d05
makdon <makdon@makdon.me>
2023-02-22 23:35:08 +0800

    init

---
+++ fetch_data.sh
@@ -0,0 +1,67 @@
+curl 'https://game.gtimg.cn/images/lol/act/jkzlk/js//8/7.3.30-S7/chess.js' \
+  -H 'authority: game.gtimg.cn' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
+  -H 'cache-control: no-cache' \
+  -H 'origin: https://jcc.qq.com' \
+  -H 'pragma: no-cache' \
+  -H 'referer: https://jcc.qq.com/' \
+  -H 'sec-ch-ua: ".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: cross-site' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36' \
+  --compressed -o ./json/chess.json
+
+curl 'https://game.gtimg.cn/images/lol/act/jkzlk/js//8/7.3.30-S7/race.js' \
+  -H 'authority: game.gtimg.cn' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
+  -H 'cache-control: no-cache' \
+  -H 'origin: https://jcc.qq.com' \
+  -H 'pragma: no-cache' \
+  -H 'referer: https://jcc.qq.com/' \
+  -H 'sec-ch-ua: ".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: cross-site' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36' \
+  --compressed -o ./json/race.json
+
+curl 'https://game.gtimg.cn/images/lol/act/jkzlk/js//8/7.3.30-S7/job.js' \
+  -H 'authority: game.gtimg.cn' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
+  -H 'cache-control: no-cache' \
+  -H 'origin: https://jcc.qq.com' \
+  -H 'pragma: no-cache' \
+  -H 'referer: https://jcc.qq.com/' \
+  -H 'sec-ch-ua: ".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: cross-site' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36' \
+  --compressed -o ./json/job.json
+
+curl 'https://game.gtimg.cn/images/lol/act/jkzlk/js//8/7.3.30-S7/trait.js' \
+  -H 'authority: game.gtimg.cn' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-GB,en-US;q=0.9,en;q=0.8' \
+  -H 'cache-control: no-cache' \
+  -H 'origin: https://jcc.qq.com' \
+  -H 'pragma: no-cache' \
+  -H 'referer: https://jcc.qq.com/' \
+  -H 'sec-ch-ua: ".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: cross-site' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36' \
+  --compressed -o ./json/trait.json
