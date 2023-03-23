4952a76e96442b2977753a1f59d1dd8a6633fb1a
makdon <makdon@makdon.me>
2023-03-24 00:12:59 +0800

    wip

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # tft-visualization
 describe tft chess and jobs in graph
+
+# Download data
+
+`sh fetch_data.sh`
+
+# Convert to neo4j data
+
+`python3 convertor.py`
--- convertor.py
+++ convertor.py
@@ -7,7 +7,18 @@ chess = json.load(open("./json/chess.json"))
 job = json.load(open("./json/job.json"))
 race = json.load(open("./json/race.json"))
 trait = json.load(open("./json/trait.json"))
-neo_graph = {}
+neo_graph = {
+    "results": [{
+        "columns": ["user", "entity"],
+        "data":[{
+            "graph": {
+                "nodes": [],
+                "relationships": []
+            }
+        }]
+    }],
+    "errors": []
+}
 
 def download_img(data_set):
     for k, data in data_set["data"].items():
@@ -15,6 +26,7 @@ def download_img(data_set):
         parsed_url = urlparse(url)
         response = requests.get(url)
         open("./img/" + os.path.basename(parsed_url.path), "wb").write(response.content)
+        print("Download " + data["name"] + "Done")
 
 def download_all_img():
     download_img(chess)
@@ -22,8 +34,64 @@ def download_all_img():
     download_img(race)
     download_img(trait)
 
-def parse_chess(all_chess):
+
+def convert_to_neo4j_data():
+    parse_chess()
+    parse_job()
+    parse_race()
+    parse_trait()
+    with open("./json/neo4jData.json", "w") as fp:
+        json.dump(neo_graph, fp)
+    with open("./neo4jd3/docs/json/neo4jData.json", "w") as fp:
+        json.dump(neo_graph, fp)
+    return
+
+def parse_chess():
+    nodes = neo_graph["results"][0]["data"][0]["graph"]["nodes"]
+    for k, data in chess["data"].items():
+        # TODO(makdon): 还需要处理羁绊
+        node = {
+            "id": data["name"],
+            "image": data["picture"],
+            "labels": [],
+            "properties": data
+        }
+        nodes.append(node)
     pass
+
+def parse_job():
+    nodes = neo_graph["results"][0]["data"][0]["graph"]["nodes"]
+    for k, data in job["data"].items():
+        node = {
+            "id": data["name"],
+            "image": data["picture"],
+            "labels": [],
+            "properties": data
+        }
+        nodes.append(node)
+
+def parse_race():
+    nodes = neo_graph["results"][0]["data"][0]["graph"]["nodes"]
+    for k, data in race["data"].items():
+        node = {
+            "id": data["name"],
+            "image": data["picture"],
+            "labels": [],
+            "properties": data
+        }
+        nodes.append(node)
+
+def parse_trait():
+    nodes = neo_graph["results"][0]["data"][0]["graph"]["nodes"]
+    for k, data in trait["data"].items():
+        node = {
+            "id": data["name"],
+            "image": data["picture"],
+            "labels": [],
+            "properties": data
+        }
+        nodes.append(node)
+
 
 if __name__ == "__main__":
     download_all_img()
