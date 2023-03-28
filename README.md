b68b321b065d1a422794e1194ae0065c76e78eb8
makdon <makdon@makdon.me>
2023-03-28 20:54:53 +0800

    v0.1.0

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ describe tft chess and jobs in graph
 This project is based on [neo4jd3][neo4jd3]. All the data is fetched from [jcc.qq.com][jcc]
 It is still under construction.
 
+# Download data
+
+`sh fetch_data.sh`
+
+# Convert to neo4j data
+
+`python3 convertor.py`
 
 [neo4jd3]: https://github.com/eisman/neo4jd3
 [jcc]https://jcc.qq.com/#/hero
---
+++ convertor.py
@@ -0,0 +1,130 @@
+import json
+import requests
+import os
+from urllib.parse import urlparse
+
+chess = json.load(open("./json/chess.json"))
+job = json.load(open("./json/job.json"))
+race = json.load(open("./json/race.json"))
+trait = json.load(open("./json/trait.json"))
+neo_graph = {
+    "results": [{
+        "columns": ["user", "entity"],
+        "data":[{
+            "graph": {
+                "nodes": [],
+                "relationships": []
+            }
+        }]
+    }],
+    "errors": []
+}
+idToNameMap = {}
+
+def download_img(data_set):
+    for k, data in data_set["data"].items():
+        url = data["picture"]
+        parsed_url = urlparse(url)
+        response = requests.get(url)
+        open("./img/" + os.path.basename(parsed_url.path), "wb").write(response.content)
+        print("Download " + data["name"] + "Done")
+
+def download_all_img():
+    download_img(chess)
+    download_img(job)
+    download_img(race)
+    download_img(trait)
+
+
+def convert_to_neo4j_data():
+    parse_job()
+    parse_race()
+    parse_trait()
+    parse_chess()
+    # neo_graph["results"][0]["data"][0]["graph"]["relationships"] = []
+    with open("./json/neo4jData.json", "w") as fp:
+        json.dump(neo_graph, fp)
+    with open("./neo4jd3/docs/json/neo4jData.json", "w") as fp:
+        json.dump(neo_graph, fp)
+    return
+
+def parse_chess():
+    nodes =         neo_graph["results"][0]["data"][0]["graph"]["nodes"]
+    relationships = neo_graph["results"][0]["data"][0]["graph"]["relationships"]
+    for k, data in chess["data"].items():
+        node = {
+            "id": data["name"],
+            "image": data["picture"],
+            "labels": [data["name"]],
+            "properties": data
+        }
+        nodes.append(node)
+        classes = data["class"].split("|")
+        for class_ in classes:
+            class_ = idToNameMap.get(class_)
+            if class_ is not None:
+                rls = {
+                    "id": data["name"] + "-" + class_,
+                    "startNode": data["name"],
+                    "endNode": class_,
+                    "type": "has_class",
+                    "properties": {
+                        "id": data["name"] + "-" + class_,
+                    }
+                }
+                relationships.append(rls)
+        species = data["species"]
+        species = idToNameMap.get(species)
+        if species is not None:
+            rls = {
+                "id": data["name"] + "-" + species,
+                "startNode": data["name"],
+                "endNode": species,
+                "type": "has_species",
+                "properties": {
+                    "id": data["name"] + "-" + species,
+                }
+            }
+            relationships.append(rls)
+
+        
+
+def parse_job():
+    nodes = neo_graph["results"][0]["data"][0]["graph"]["nodes"]
+    for k, data in job["data"].items():
+        node = {
+            "id": data["name"],
+            "image": data["picture"],
+            "labels": [],
+            "properties": data
+        }
+        nodes.append(node)
+        idToNameMap[data["id"]] = data["name"]
+
+def parse_race():
+    nodes = neo_graph["results"][0]["data"][0]["graph"]["nodes"]
+    for k, data in race["data"].items():
+        node = {
+            "id": data["name"],
+            "image": data["picture"],
+            "labels": [],
+            "properties": data
+        }
+        nodes.append(node)
+        idToNameMap[data["id"]] = data["name"]
+
+def parse_trait():
+    nodes = neo_graph["results"][0]["data"][0]["graph"]["nodes"]
+    for k, data in trait["data"].items():
+        node = {
+            "id": data["name"],
+            "image": data["picture"],
+            "labels": [],
+            "properties": data
+        }
+        nodes.append(node)
+        idToNameMap[data["id"]] = data["name"]
+
+
+if __name__ == "__main__":
+    convert_to_neo4j_data()
